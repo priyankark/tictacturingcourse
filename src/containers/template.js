aed7bdf3ba714290e67403212e0a06d809ed65c2
Priyankar Kumar <priyankar.kumar98@gmail.com>
2017-12-07 01:05:41 +0530

    Heroku try

--- src/containers/template.js
+++ src/containers/template.js
@@ -1,23 +1,26 @@
 import React,{Component} from 'react';
-//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-//import injectTapEventPlugin from 'react-tap-event-plugin';
-
-///injectTapEventPlugin()
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+injectTapEventPlugin()
 
 class Template extends Component{
 
   render(){
     return(
-
+    <MuiThemeProvider>
       <div>
         <header>
           <h1>TicTacTuring </h1>
+          <RaisedButton primary={true}
+           label='Test Button' onTouchTap={()=>{console.log("Hello I work")}} />
         </header>
         <main>
           {this.props.children}
 
         </main>
       </div>
+    </MuiThemeProvider>
 
 
 
